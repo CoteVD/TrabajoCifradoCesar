4fc262db5601654cea882e74f354bb3471a90602
Cote <majovardon@gmail.com>
2018-05-25 12:44:01 -0400

    CSS y HTML hasta ahora

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 /* Acá va tu código */
+const btnC = document.getElementById('btnC');
+
+btnC.addEventListener("click", function (){
+    const letters = ['A', 'B'];
+        for (let i = 0; i < nombres.length; i++) {
+            console.log(nombres[i]);
+       }
+
+    });
+
+const btnT = document.getElementById('btnT');
+
+btnT.addEventListener("click", function (){
+    
+})
