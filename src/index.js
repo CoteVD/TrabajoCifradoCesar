b45683ee103acf4be2613a45ee4e9114543510a2
Cote <majovardon@gmail.com>
2018-05-28 19:38:35 -0400

    Agregue los DOM de los botones

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 /* Acá va tu código */
 const btnC = document.getElementById('btnC');
-
-btnC.addEventListener("click", function (){
-    const letters = ['A', 'B'];
-        for (let i = 0; i < nombres.length; i++) {
-            console.log(nombres[i]);
-       }
-
-    });
+btnC.addEventListener("click", function (){})
 
 const btnT = document.getElementById('btnT');
-
-btnT.addEventListener("click", function (){
-    
-})
+btnT.addEventListener("click", function (){})
