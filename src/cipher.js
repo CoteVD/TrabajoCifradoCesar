5aba3cc1edbeaaba5b545ff44a524620faf889ac
Cote <majovardon@gmail.com>
2018-05-30 21:33:57 -0400

    Programa traduce sin problema, pero descifra solo en mayusc

--- src/cipher.js
+++ src/cipher.js
@@ -14,5 +14,13 @@ window.cipher = {
 
   decode: (text, shifts) => {
     /* Acá va tu código */
+    let cipDecode = ""; // Aquí se almacenan las letras reemplazadas, va a ser el nuevo mensaje.
+    for (var i = 0; i < text.length; i++) { // La función va recorriendo cada letra del string hasta llegar a la última.
+      let txt = text.charCodeAt(i); // Aquí se traducen de letras a unicodes. 
+        if (65 <= txt && txt <= 90) { // Aquí se indica el caso de una letra. 
+          cipDecode += String.fromCharCode ((txt + 65 - parseInt(shifts))% 26 + 65);// Aquí se aplica la fórmula para obtener el nuevo número, y traducir la letra del mensaje. Por el momento sólo puedo traducir con mayúsculas porque las minúsculas me están dando problemas.  
+        } else {cipDecode += text.charAt(i)}; // Aquí se aplica el caso en que no sean letras. Se copia directamente el carácter.
+    } return document.getElementById("trad-text").innerHTML = cipDecode; // Retorna el nuevo mensaje decodificado.
   }
-}
+} 
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 /* Acá va tu código */
 window.onload = function() {
-
-const btnC = document.getElementById('btnC');
-btnC.addEventListener("click", function (){
-  let text = document.getElementById("source-text").value;
-  let shifts = document.getElementById("shiftNumber").value;
-  window.cipher.encode(text, shifts);
+// Botón para la función de cifrar.
+const btnC = document.getElementById('btnC'); // Llama al botón del HTML.
+btnC.addEventListener("click", function (){ // Al botón se le adicióna el click y lo que sucede cuando éste evento sucede.
+  let text = document.getElementById("source-text").value; // Se guarda el valor de "source-text" en la variable "text".
+  let shifts = document.getElementById("shiftNumber").value; // Se guara el valor de "shiftNumber" en la variable "shifts".
+  window.cipher.encode(text, shifts); // Se mandan los valores obtenidos a la función encode.
 });
 
-const btnT = document.getElementById('btnT');
-btnT.addEventListener("click", function (){
-  let text = document.getElementById("source-text").value;
-  let shifts = document.getElementById("shiftNumber").value;
-  window.cipher.decode(text, shifts);
+//Botón para la función descifrar.
+const btnT = document.getElementById('btnT'); // Llama al botón del HTML.
+btnT.addEventListener("click", function (){ // Al botón se le adicióna el click y lo que sucede cuando éste evento sucede.
+  let text = document.getElementById("source-text").value; // Se guarda el valor de "source-text" en la variable "text".
+  let shifts = document.getElementById("shiftNumber").value; // Se guara el valor de "shiftNumber" en la variable "shifts".
+  window.cipher.decode(text, shifts); // Se mandan los valores obtenidos a la función encode.
 });
 }
